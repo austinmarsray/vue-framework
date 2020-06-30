e06288f6ea7ff1f5277319d88aee664706003777
austinmarsray <austinmray@outlook.com>
2020-06-30 21:25:05 +0800

    多次操作后websocket连接缓慢，录入DamageType时路的类型需改为后端确定

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+        <router-view></router-view>
+</template>
+
+<script>
+    export default{
+      name: 'APP',
+            created() {
+              if (sessionStorage.getItem('verification') !== ''){
+                      this.$store.commit('EventListener');
+              }
+            }
+    }
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="app-footer">
+    <a >LHX</a> &copy; 2020 creativeLabs.
+    <span class="float-right">Modified by <a href="http://austinmarsray.io">LHX</a></span>
+    <span class="float-right">Powered by <a href="http://coreui.io">WZ</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
